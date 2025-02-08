6fb5c8c264e997d7394e0a4b2a87aabe5f5c8343
Suyog Dhepe <153749895+suyog657898775456@users.noreply.github.com>
2025-02-08 20:28:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import nltk
+from transformers import pipeline
+from nltk.tokenize import word_tokenize
+
+chatbot = pipeline("text-generation", model="distilbert/distilgpt2")
+
+def healthcare_chatbot(user_input):
+    if "symptom" in user_input:
+        return "Please Consult Doctor for accurate advice"
+    elif "appointment" in user_input:
+        return "would you like to schedule appointment with doctor?"
+    elif "medication" in user_input:
+        return "its important to take prescribed medicines regularly. if you have cocerns, consult your doctor"
+    elif "fever" in user_input:
+        return "its important to take prescribed medicines regularly. if you have cocerns, consult your doctor"
+    else :
+        response = chatbot(user_input, max_length=500, num_return_sequences=1)
+        return response[0]['generated text']
+
+def main():
+    st.title("Healthcare Assistant Chatbot")
+    user_input=st.text_input("How I can Assist you today?")
+    if st.button("Submit"):
+        if user_input:
+            st.write("User : ", user_input)
+            with st.spinner("processing your query, please wait...."):
+                response = healthcare_chatbot(user_input)
+            st.write("Healthcare Assistant :",response)
+            print(response)
+    else:
+             st.write("Please enter message to get a response.")
+
+main()
+
+
+
